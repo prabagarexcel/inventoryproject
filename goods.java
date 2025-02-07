a64b34383dd6f1aec06bc96d1a219af7bbbe6a3b
prabagarexcel <prabagarexcel@gmail.com>
2025-02-07 15:25:01 +0530

    added the new content

--- goods.java
+++ goods.java
@@ -2,4 +2,5 @@ public static void main()
   {
   System.out.println("This program for goods purchase in purchase module");
   System.out.println("This is updated goods purchase module");
+  System.out.println("Next updata");
 }
